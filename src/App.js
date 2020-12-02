87bc89581c3cec2679d3b3fbfab22cb0e973f794
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2020-12-02 07:17:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Component/Pages/Home";
 import Profile from "./Component/Pages/Profile";
 import NavbarComponent from "./Component/NavbarComponent";
